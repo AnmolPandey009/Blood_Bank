1638ac1b0bf49ab121a795897494c884b4b80b35
AnmolPandey009 <pandeyanmol0909@gmail.com>
2025-08-25 12:22:34 +0530

    Frontend update

---
+++ Frontend_Project/Blood Bank/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+
+export default function Header(){
+
+    return(
+        <>
+            <div className="flex justify-between bg-red-400 py-4">
+                <div className="px-4">IMG</div>
+                <div className="flex">
+                    <button className="px-4">HOME</button>
+                    <button className="px-4">LOOKING FOR BLOOD</button>
+                    <button className="px-4">WAIT TO DONATE BLOOD</button>
+                    <button className="px-4">BLOOD CENTER LOGIN</button>
+                </div>
+            </div>
+        </>
+    )
+}
