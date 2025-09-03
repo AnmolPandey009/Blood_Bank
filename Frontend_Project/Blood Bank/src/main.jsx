53c64b21e4330fbb1a09d1ad873f326b899999de
Abhishek Saini <abhisheksaini200404@gmail.com>
2025-09-03 12:21:15 +0530

    Fixed navbar issue in header

--- Frontend_Project/Blood Bank/src/main.jsx
+++ Frontend_Project/Blood Bank/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Header from './Components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header></Header>
-    {/* <App /> */}
+    <BrowserRouter>
+      <Header />
+      {/* <App /> */}
+    </BrowserRouter>
   </StrictMode>,
 )
